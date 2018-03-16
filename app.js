7804be4c47544f2c3ca110f984a055e630a88783
kika-lixiang <lex.li@kikatech.com>
2018-03-16 17:49:02 +0800

    1

---
+++ app.js
@@ -0,0 +1,12 @@
+function handleShare() { 
+  document.getElementById('share').style.display = "block"
+}
+
+function handleRetrun() { 
+  console.log('返回')
+  document.getElementById('share').style.display = "none"
+  // f()
+}
+
+
+        

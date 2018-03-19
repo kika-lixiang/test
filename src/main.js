19fccca0a560b4f0612ff94251e34abf1704aa9f
kika-lixiang <lex.li@kikatech.com>
2018-03-19 18:48:37 +0800

    http://www.tcyclops.com/both-survive/gdyx.png

--- src/main.js
+++ src/main.js
@@ -603,8 +603,8 @@ var link, jsGame;
   require(["lib/link", "index"], 
     function(e, t) { 
       e.init(window.innerWidth, window.innerHeight).pushImage([
-        { id: "logo", src: "//www.tcyclops.com/both-survive/ygdbns.jpg" }, 
-        { id: "btns1", src: "//game.ioxapp.com/nodie/img/btns1.png" }], 
+        { id: "logo", src: "//game.ioxapp.com/nodie/img/logo-big.jpg" }, 
+        { id: "btns1", src: "http://www.tcyclops.com/both-survive/gdyx.png" }], 
         function(e, t, n) {}).initAudio([
         { id: "1", src: "sound/1.mp3", preload: !0 }
         ]).loadingCallBack(

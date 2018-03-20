43706c8c8e0046859d887f40bcdb66c4d5d2db56
kika-lixiang <lex.li@kikatech.com>
2018-03-20 16:54:22 +0800

    data.js

--- Basketball_files/c2runtime.js
+++ Basketball_files/c2runtime.js
@@ -3901,7 +3901,7 @@ quat4.str = function(a) { return "[" + a[0] + ", " + a[1] + ", " + a[2] + ", " +
           "localhost_only": true
         }, function(url) {
           self.httpServerUrl = url;
-          self.fetchLocalFileViaCordovaAsText("http://kiz10.com/upload/games/htmlgames/crazy-basket-2016/1480036260/data.js", function(str) {
+          self.fetchLocalFileViaCordovaAsText("data.js", function(str) {
             self.loadProject(JSON.parse(str));
           }, function(err) {
             alert("Error fetching data.js");
@@ -3910,7 +3910,7 @@ quat4.str = function(a) { return "[" + a[0] + ", " + a[1] + ", " + a[2] + ", " +
           alert("error starting local server: " + err);
         });
       } else {
-        this.fetchLocalFileViaCordovaAsText("http://kiz10.com/upload/games/htmlgames/crazy-basket-2016/1480036260/data.js", function(str) {
+        this.fetchLocalFileViaCordovaAsText("data.js", function(str) {
           self.loadProject(JSON.parse(str));
         }, function(err) {
           alert("Error fetching data.js");
@@ -3923,7 +3923,7 @@ quat4.str = function(a) { return "[" + a[0] + ", " + a[1] + ", " + a[2] + ", " +
       xhr = new ActiveXObject("Microsoft.XMLHTTP");
     else
       xhr = new XMLHttpRequest();
-    var datajs_filename = "http://kiz10.com/upload/games/htmlgames/crazy-basket-2016/1480036260/data.js";
+    var datajs_filename = "data.js";
     if (this.isWindows8App || this.isWindowsPhone8 || this.isWindowsPhone81 || this.isWindows10)
       datajs_filename = "data.json";
     xhr.open("GET", datajs_filename, true);
